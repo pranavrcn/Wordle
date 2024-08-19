c48fef83e303f414aa7d00202ffd2fcc55a13a32
Pranav Ravichandran <121128196+pranavrcn@users.noreply.github.com>
2024-08-19 14:04:05 -0400

    Update LetterResult.java

--- src/main/java/edu/virginia/sde/hw2/wordle/LetterResult.java
+++ src/main/java/edu/virginia/sde/hw2/wordle/LetterResult.java
@@ -3,6 +3,6 @@ package edu.virginia.sde.hw2.wordle;
 
 public enum LetterResult {
     GREEN,
-    YELLOW, //Correct Letter, but incorrect location
-    GRAY //Incorrect Letter
+    YELLOW,
+    GRAY 
 }
