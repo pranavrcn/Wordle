f0043f39a2050bb210fc72eee8874cc1b5b231b0
Pranav Ravichandran <121128196+pranavrcn@users.noreply.github.com>
2024-08-19 14:01:10 -0400

    Update GameStatus.java

--- src/main/java/edu/virginia/sde/hw2/wordle/GameStatus.java
+++ src/main/java/edu/virginia/sde/hw2/wordle/GameStatus.java
@@ -1,20 +1,10 @@
 package edu.virginia.sde.hw2.wordle;
 
-/**
- * This enum describes the current state of the World game, either {@link GameStatus#PLAYING},
- * {@link GameStatus#WIN}, or {@link GameStatus#LOSS},
- */
 public enum GameStatus {
-    /**
-     * The game is ongoing
-     */
+
     PLAYING,
-    /**
-     * The game has ended in a loss
-     */
+
     LOSS,
-    /**
-     * The game has ended in a win
-     */
+
     WIN
 }
